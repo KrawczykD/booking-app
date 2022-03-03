6c278dba48efef81338f474b7bbf86c9e3293e4e
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2022-03-03 14:20:18 +0000

    add interfaces

---
+++ src/store/ICategory.ts
@@ -0,0 +1,6 @@
+export default interface ICategory {
+  title: string;
+  isActive: boolean;
+  id: number;
+  color: string;
+}
---
+++ src/store/IProduct.ts
@@ -0,0 +1,11 @@
+export default interface IProduct {
+  title: string;
+  description: string;
+  id: number;
+  categoryId: number;
+  imgsrc: string;
+  price: number;
+  alt: string;
+  orderedQty: number;
+  maxQty: number;
+}
