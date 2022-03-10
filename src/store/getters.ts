a1beb2e9b223841fe74d07ddeb46f91083f3a6fd
KrawczykD <40856421+KrawczykD@users.noreply.github.com>
2022-03-10 17:24:47 +0000

    remove console.log

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -3,13 +3,8 @@ import IProduct from './IProduct';
 export const getCartValue = function (cart: Array<IProduct>): number {
   let cartValue = 0;
 
-  console.log(cart);
-
   cart.forEach((item) => {
     cartValue += item.price * item.orderedQty;
   });
-
-  console.log(cartValue);
-
   return cartValue;
 };
